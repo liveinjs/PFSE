c271b213be49244305b98915b8f47de7505b7415
Wang Liang <liveinjs.com@gmail.com>
2017-04-26 15:37:19 +0800

    Update app.R

--- app.R
+++ app.R
@@ -1,5 +1,11 @@
 library(compiler)
 loadcmp("POW.Rc")
 
-#doing(a,b,c) a 要生成的图片数量，b 相关度，c 股票代码
-doing(3,0.3,601719)
+#pow(a,b,c,d) 参数说明
+#a，要生成的图片数量
+#b，相关度
+#c，股票代码
+#d，样本数据路径,样本数据格式为tab分割的文本文件，收盘价格列名请改为 Close。
+
+#example
+pow(3,0.5,601718,"./ExpData.TXT")
